fa025cd3168cbbcf08162060bc4bdc58e7511ab8
Jhonathan Henrique <Jhonathan.Engineer@gmail.com>
2025-02-22 19:03:10 -0300

    chore: add ficha de participante

---
+++ fichaparticipante/bin/fichaparticipante.dart
@@ -0,0 +1,9 @@
+void main() {
+  var nome = "Jhonathan";
+  var cpf = "161.820.827.63";
+  int idade = 29;
+  double altura = 1.90;
+  bool comunidade = true;
+
+print("Eu sou $nome, \nMeu CPF é: $cpf, \nSou membro da comunidade? $comunidade, \nTenho $altura de altura e \n$idade de idade.");
+}
---
+++ fichaparticipante/test/fichaparticipante_test.dart
@@ -0,0 +1,8 @@
+import 'package:fichaparticipante/fichaparticipante.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
